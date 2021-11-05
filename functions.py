0a80c945e22f6bfc0704e698fb8614e13483ba02
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-11-05 23:46:33 +0000

    functions

--- functions.py
+++ functions.py
@@ -37,4 +37,30 @@ def coin_flip():
         print("Error, please try again")
 
 coin_flip()
-    
+
+
+# Create a function that accepts a string and returns the same string reversed.
+def reverse():
+    answer = input("What word would you like to reverse? ")
+    txt = answer[::-1]
+    return("Your word reversed is: " + txt)
+
+print(reverse())
+
+# Create a function that simulates a pair of dice being rolled n times
+# and returns the number of occurrences of each score.
+def dice_roll(n):
+    import random
+    dice1 = 0
+    dice2 = 0
+    occurrence = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for i in range(0, n):
+        dice1 = random.randrange(1, 7)
+        if dice1 in occurrence:
+            occurrence[dice1] +=1
+        dice2 = random.randrange(1, 7)
+        if dice2 in occurrence:
+            occurrence[dice2] +=1
+    return occurrence
+        
+print(dice_roll(1))
