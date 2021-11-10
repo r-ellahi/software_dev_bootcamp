a1255cd28187b3ab593643ef9540bfe0b611791f
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-11-10 22:42:46 +0000

    progress update

--- lists.py
+++ lists.py
@@ -29,8 +29,57 @@
 # print(topic[2:12])
 
 # # LISTS 4 
-teams = ["WOLVES", "OWLS", "PANTHERS", "BEARS", "DRAGONS"]
+# teams = ["WOLVES", "OWLS", "PANTHERS", "BEARS", "DRAGONS"]
+# teams_short = []
 
-team_short = teams[0:3]
+# for team in teams:
+#     teams_short.append(team[:3])
 
-print(team_short)
+# print(teams_short)
+
+# # LISTS 4
+# players = ["Spinelli", "Gretchen", "Mickey", "T.J.", "Gus", "Vince"]
+# player_short = []
+
+# for i in players:
+#     player_short.append(i[:3])
+
+# print(player_short)
+
+
+# LISTS 5 - PSEUDOCODE FOLLOWED BY WORKING CODE
+
+
+# PSEUDOCODE:
+count <- 0
+
+FOR item <- 0 to LEN(drinks)
+  IF item = “Tea” THEN
+    count <- count + 1
+ENDFOR
+
+OUTPUT(count)
+
+
+# WORKING CODE:
+
+# drinks = ["Tea", "Coffee", "Tea", "Tea", "Coffee", "Hot Chocolate", "Tea"]
+# new_list = []
+
+# for drink in drinks:
+#     if drink == "Tea":
+#         new_list = new_list +1
+# print(int(new_list))
+
+
+drinks = ["Tea", "Coffee", "Tea", "Tea", "Coffee", "Hot Chocolate", "Tea"]
+
+# Dictionary count of drinks
+result = dict((i, drinks.count(i)) for i in drinks)
+print(result)
+
+# Count occurrence of repeated element within list 
+repeated_tea = drinks.count("Tea")
+print(repeated_tea)
+
+
