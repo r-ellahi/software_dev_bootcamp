4ee5303b2a93ef657f0792e738e0303abe8c2817
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-11-14 19:27:35 +0000

    work

--- functions.py
+++ functions.py
@@ -1,171 +1,171 @@
-# #Create a haiku function, which prints out each line of a haiku one by one.
-# def haiku():
-#     import time
-#     print("I am very tired,")
-#     time.sleep(1.0)
-#     print("But still I code even though,")
-#     time.sleep(1.0)
-#     print("I am very tired")
+#Create a haiku function, which prints out each line of a haiku one by one.
+def haiku():
+    import time
+    print("I am very tired,")
+    time.sleep(1.0)
+    print("But still I code even though,")
+    time.sleep(1.0)
+    print("I am very tired")
     
-# haiku()
+haiku()
 
-# # Create a function that asks the user for the height and base of a triangle, 
-# # and then calculates and prints out its area.
-# def triangle():
-#     height = float(input("What is the height of the triangle? "))
-#     base = float(input("What is the base of the triangle? "))
-#     area = (height * base) / 2
-#     print(area)
+# Create a function that asks the user for the height and base of a triangle, 
+# and then calculates and prints out its area.
+def triangle():
+    height = float(input("What is the height of the triangle? "))
+    base = float(input("What is the base of the triangle? "))
+    area = (height * base) / 2
+    print(area)
 
-# triangle()
+triangle()
 
-# # Create function that simulates a coin flip. 
-# # Each time the function is called, it should randomly select either heads or tails and print it out
-# def coin_flip():
-#     import random
-#     import time
-#     flip = random.randint(0,1)
+# Create function that simulates a coin flip. 
+# Each time the function is called, it should randomly select either heads or tails and print it out
+def coin_flip():
+    import random
+    import time
+    flip = random.randint(0,1)
     
-#     guess = input("Heads or Tails? ")
+    guess = input("Heads or Tails? ")
     
-#     time.sleep(1.5)
-#     if flip == 0:
-#         print("Heads, well done!")
-#     elif flip == 1:
-#         print("Tails")
-#     else:
-#         print("Error, please try again")
+    time.sleep(1.5)
+    if flip == 0:
+        print("Heads, well done!")
+    elif flip == 1:
+        print("Tails")
+    else:
+        print("Error, please try again")
 
-# coin_flip()
+coin_flip()
 
 
-# # Create a function that accepts a string and returns the same string reversed.
-# def reverse():
-#     answer = input("What word would you like to reverse? ")
-#     txt = answer[::-1]
-#     return("Your word reversed is: " + txt)
+# Create a function that accepts a string and returns the same string reversed.
+def reverse():
+    answer = input("What word would you like to reverse? ")
+    txt = answer[::-1]
+    return("Your word reversed is: " + txt)
 
-# print(reverse())
+print(reverse())
 
-# # Create a function that simulates a pair of dice being rolled n times
-# # and returns the number of occurrences of each score.
-# def dice_roll(n):
-#     import random
-#     dice1 = 0
-#     dice2 = 0
-#     occurrence = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
-#     for i in range(0, n):
-#         dice1 = random.randrange(1, 7)
-#         if dice1 in occurrence:
-#             occurrence[dice1] +=1
-#         dice2 = random.randrange(1, 7)
-#         if dice2 in occurrence:
-#             occurrence[dice2] +=1
-#     return occurrence
+# Create a function that simulates a pair of dice being rolled n times
+# and returns the number of occurrences of each score.
+def dice_roll(n):
+    import random
+    dice1 = 0
+    dice2 = 0
+    occurrence = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for i in range(0, n):
+        dice1 = random.randrange(1, 7)
+        if dice1 in occurrence:
+            occurrence[dice1] +=1
+        dice2 = random.randrange(1, 7)
+        if dice2 in occurrence:
+            occurrence[dice2] +=1
+    return occurrence
         
-# print(dice_roll(1))
+print(dice_roll(1))
 
-# # TURTLE 1
-# from turtle import Turtle 
-# tina = Turtle()
+# TURTLE 1
+from turtle import Turtle 
+tina = Turtle()
 
-# def draw_circle(name, r, color, x, y):
-#     name.penup()
-#     name.goto(x, y)
-#     name.dot(2 * r)
-#     name.color(color)
+def draw_circle(name, r, color, x, y):
+    name.penup()
+    name.goto(x, y)
+    name.dot(2 * r)
+    name.color(color)
     
 
-# draw_circle(tina, 20, "pink", 20, 20)
-# #First line of code ^ prints as black on  console - not sure if its an issue with console or code
-# # remainder of code prints in colours listed except for last line 
-# draw_circle(tina, 40, "orange", -100, -100)
-# draw_circle(tina, 60, "yellow", -200, -200)
-# draw_circle(tina, 40, "lime", 250, 200)
+draw_circle(tina, 20, "pink", 20, 20)
+#First line of code ^ prints as black on  console - not sure if its an issue with console or code
+# remainder of code prints in colours listed except for last line 
+draw_circle(tina, 40, "orange", -100, -100)
+draw_circle(tina, 60, "yellow", -200, -200)
+draw_circle(tina, 40, "lime", 250, 200)
 
 
 
-# #TURTLE 2
-# from turtle import Turtle 
-# tina = Turtle()
+#TURTLE 2
+from turtle import Turtle 
+tina = Turtle()
 
-# def circle_area(r):
-#     return 3.14 * r * r
+def circle_area(r):
+    return 3.14 * r * r
 
-# def circle_circumference(r):
-#     return 2 * 3.14 * r 
+def circle_circumference(r):
+    return 2 * 3.14 * r 
     
 
-# def draw_circle(name, r, color):
-#     name.color(color)
-#     name.dot(2 * r)
+def draw_circle(name, r, color):
+    name.color(color)
+    name.dot(2 * r)
     
-#     name.color("black")
-#     name.goto(0,5)
-#     name.write("area: " + str(circle_area(r)), align="center")
-#     name.goto(0, -5)
-#     name.write("circ: " + str(circle_circumference(r)))
-#     name.hideturtle()
+    name.color("black")
+    name.goto(0,5)
+    name.write("area: " + str(circle_area(r)), align="center")
+    name.goto(0, -5)
+    name.write("circ: " + str(circle_circumference(r)))
+    name.hideturtle()
 
-# draw_circle(tina, 100, "blue")
+draw_circle(tina, 100, "blue")
 
 
-# import turtle
+import turtle
 
-# def cross():
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.left(90)
+def cross():
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
 
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.left(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
     
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.left(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
     
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.right(90)
-#     turtle.forward(10)
-#     turtle.left(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
 
-# cross()
+cross()
 
-# def tip(total, percentage):
-#     new_total = total / 100
-#     return (new_total * percentage)
+def tip(total, percentage):
+    new_total = total / 100
+    return (new_total * percentage)
 
-# print(tip(100, 25))
+print(tip(100, 25))
 
-# def dog_years(name, age):
-#   return (name + " you are " + str(age *7) + " years old in dog years")
+def dog_years(name, age):
+  return (name + " you are " + str(age *7) + " years old in dog years")
 
-# print(dog_years("eggs", 3))
+print(dog_years("eggs", 3))
 
 
-# def lots_of_math(a, b, c, d):
-#     first = a + b
-#     second = c - d
-#     third = first * second
-#     fourth = third % a
-#     print(first)
-#     print(second)
-#     print(third)
-#     return fourth
+def lots_of_math(a, b, c, d):
+    first = a + b
+    second = c - d
+    third = first * second
+    fourth = third % a
+    print(first)
+    print(second)
+    print(third)
+    return fourth
 
-# print(lots_of_math(1, 2, 3, 4))
+print(lots_of_math(1, 2, 3, 4))
 
 def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
     if (budget < food_bill + electricity_bill + internet_bill+ rent):
@@ -174,3 +174,35 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
         return False
 
 print(over_budget(100, 20, 30, 10, 40))
+
+
+def append_sum(lst):
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    return lst
+
+print(append_sum([1, 1, 2]))
+
+
+def larger_list(lst1, lst2):
+    if len(lst1) >= len(lst2):
+        return lst1[-1]
+    else:
+        return lst2[-1]
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+
+def more_than_n(lst, item, n):
+    if lst.count(item) > n:
+        return True
+    else:
+        return False
+
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+def combine_sort(lst1, lst2):
+    return sorted(lst1 + lst2)
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
